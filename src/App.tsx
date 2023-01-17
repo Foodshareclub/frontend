f28bb9c99872421ffdec044a484559aadce2ad8f
Denisssska <yarmoshkoden18m@gmail.com>
2023-01-17 13:12:17 +0300

    delete login reg

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ function App() {
     const dispatch = useAppDispatch()
     const [session, setSession] = useState<Session | null>(null)
 
+
     useEffect(() => {
         supabase.auth.getSession().then(({data: {session}}) => setSession(session));
         supabase.auth.onAuthStateChange((event, session) => setSession(session));
