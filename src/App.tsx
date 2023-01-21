2e02849e10b5da3a4b9ef18de77325f6ed4e28db
Stanislav Lisovskiy <stas-lisovskii@mail.ru>
2023-01-21 18:26:02 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import {Route, Routes} from "react-router-dom";
-import ProductPage from "./pages/productPage/ProductPage";
-import {Card, CardBody} from "@chakra-ui/react";
-import AboutUsPage from "./pages/aboutUs/AboutUsPage";
-import ContactUsPage from "./pages/contactUs/ContactUsPage";
-import VolunteerPage from "./pages/volunteerPages/VolunteerPage";
-import OpportunitiesPage from "./pages/volunteerPages/OpportunitiesPage";
-import MyListingsPage from "./pages/prfilePages/MyListingsPage";
 import {useAppDispatch} from "./hook/hooks";
 import {getSession} from "./store/slices/userReducer";
 import {Session} from "@supabase/supabase-js";
 import {supabase} from "./supaBase.config";
 import {getAllProductsTC} from "./store/slices/foodReducer";
 import ChangeLanguageContainer from "./components/localization/ChangeLanguageContainer";
-
-import {Main} from "./components/main/Main";
-
 
 
 function App() {
