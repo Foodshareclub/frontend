7f368a6da78eecd90f8ef19892040837d4ba6cf3
Denisssska <yarmoshkoden18m@gmail.com>
2023-01-23 20:44:32 +0300

    refactor main

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -7,10 +7,8 @@ import {ArrowForwardIcon} from "@chakra-ui/icons";
 import useMediaQuery from '../../utils/useMediaQuery';
 import {getProductTC, InitialProductStateType} from "../../store/slices/productReducer";
 import {useAppDispatch, useAppSelector} from "../../hook/hooks";
-import veget from "../../assets/veget.png"
 import {navigatePhotosObject} from "../../utils/navigatePhotosObject";
 import {Trans} from "@lingui/macro";
-import OneCardOfMain from "./oneCardOfMain/oneCardOfMain";
 
 type MainType = {
     productType: string
@@ -18,7 +16,6 @@ type MainType = {
 
 export const Main: React.FC<MainType> = ({productType}) => {
     const dispatch = useAppDispatch();
-
 
 
     const [isLoaded, setIsLoaded] = useState(false);
@@ -56,7 +53,7 @@ export const Main: React.FC<MainType> = ({productType}) => {
     };
 
     return (
-        <Box mt="22vh" >
+        <Box mt="22vh">
             {/*<Trans>*/}
             {/*    Last login on {i18n.date(new Date())}.*/}
             {/*</Trans>*/}
@@ -64,58 +61,58 @@ export const Main: React.FC<MainType> = ({productType}) => {
                         spacing={10}>
                 {products.map((item, id) => (
                     <GridItem mt='2' mb='2' key={id}>
-                        <OneCardOfMain item={item}/>
-                        {/*<Skeleton isLoaded={isLoaded}>*/}
 
-                        {/*    <Image width="100%" height={250} cursor="pointer" borderRadius="10px"*/}
-                        {/*           onClick={() => navigate(`/oneProd`, {state: item})} src={item.gif_url}*/}
-                        {/*           alt="soup"/>*/}
-                        {/*</Skeleton>*/}
-                        {/*{!isLoaded*/}
-                        {/*    ? <div>*/}
-                        {/*        <Skeleton mt={4} height='20px' isLoaded={isLoaded}/>*/}
-                        {/*        <Skeleton mt={2} height='20px' isLoaded={isLoaded}/>*/}
-                        {/*        <Skeleton mt={2} height='20px' isLoaded={isLoaded}/>*/}
-                        {/*        <Skeleton mt={2} height='20px' isLoaded={isLoaded}/>*/}
-                        {/*    </div>*/}
-                        {/*    :*/}
-                        {/*    <Box mt={3}>*/}
-                        {/*        <Flex justify="space-between" alignItems="center" alignSelf="center" fontSize={25}>*/}
-                        {/*            <Box noOfLines={1}  fontWeight={700}>*/}
-                        {/*                {item.post_name}*/}
-                        {/*            </Box>*/}
-                        {/*            <Image*/}
-                        {/*                borderRadius='full'*/}
-                        {/*                src={navigatePhotosObject[item.post_type]}*/}
-                        {/*                alt={soup}*/}
-                        {/*            />*/}
-                        {/*        </Flex>*/}
-                        {/*        <Flex justify="space-between" alignItems="center" alignSelf="center">*/}
-                        {/*            <div style={{fontWeight: "700", fontSize: "16px"}}><Trans>Distance:</Trans></div>*/}
-                        {/*            <Box mt='1' ml="2" fontWeight='normal' as='h4' lineHeight='tight' noOfLines={1}*/}
-                        {/*            >*/}
-                        {/*                {item.post_address}*/}
-                        {/*            </Box>*/}
-                        {/*            <Image  borderRadius='full'  src={navIcon} alt={navIcon}*/}
-                        {/*            />*/}
-                        {/*        </Flex>*/}
-                        {/*        <Box display='flex' alignItems='baseline'>*/}
-                        {/*            <div style={{fontWeight: "700", fontSize: "16px"}}><Trans>Available:</Trans></div>*/}
-                        {/*            <Box mt='1' ml="2" fontWeight='normal' as='h4' lineHeight='tight' noOfLines={1}*/}
-                        {/*            >*/}
-                        {/*                {item.pickup_time}*/}
-                        {/*            </Box>*/}
-                        {/*        </Box>*/}
+                        <Skeleton isLoaded={isLoaded}>
 
-                        {/*        <Box display='flex' alignItems='baseline'>*/}
-                        {/*            <div style={{fontWeight: "700", fontSize: "16px"}}><Trans>About:</Trans></div>*/}
-                        {/*            <Box ml={2} mt='1' fontWeight='normal' as='h4' lineHeight='tight' noOfLines={1}*/}
-                        {/*            >*/}
-                        {/*                {item.post_description}*/}
-                        {/*            </Box>*/}
-                        {/*        </Box>*/}
-                        {/*    </Box>*/}
-                        {/*}*/}
+                            <Image width="100%" height={250} cursor="pointer" borderRadius="10px"
+                                   onClick={() => navigate(`/oneProd`, {state: item})} src={item.gif_url}
+                                   alt="soup"/>
+                        </Skeleton>
+                        {!isLoaded
+                            ? <div>
+                                <Skeleton mt={4} height='20px' isLoaded={isLoaded}/>
+                                <Skeleton mt={2} height='20px' isLoaded={isLoaded}/>
+                                <Skeleton mt={2} height='20px' isLoaded={isLoaded}/>
+                                <Skeleton mt={2} height='20px' isLoaded={isLoaded}/>
+                            </div>
+                            :
+                            <Box mt={3}>
+                                <Flex justify="space-between" alignItems="center" alignSelf="center" fontSize={25}>
+                                    <Box noOfLines={1} fontWeight={700}>
+                                        {item.post_name}
+                                    </Box>
+                                    <Image
+                                        borderRadius='full'
+                                        src={navigatePhotosObject[item.post_type]}
+                                        alt={soup}
+                                    />
+                                </Flex>
+                                <Flex justify="space-between" alignItems="center" alignSelf="center">
+                                    <div style={{fontWeight: "700", fontSize: "16px"}}><Trans>Distance:</Trans></div>
+                                    <Box mt='1' ml="2" fontWeight='normal' as='h4' lineHeight='tight' noOfLines={1}
+                                    >
+                                        {item.post_address}
+                                    </Box>
+                                    <Image borderRadius='full' src={navIcon} alt={navIcon}
+                                    />
+                                </Flex>
+                                <Box display='flex' alignItems='baseline'>
+                                    <div style={{fontWeight: "700", fontSize: "16px"}}><Trans>Available:</Trans></div>
+                                    <Box mt='1' ml="2" fontWeight='normal' as='h4' lineHeight='tight' noOfLines={1}
+                                    >
+                                        {item.pickup_time}
+                                    </Box>
+                                </Box>
+
+                                <Box display='flex' alignItems='baseline'>
+                                    <div style={{fontWeight: "700", fontSize: "16px"}}><Trans>About:</Trans></div>
+                                    <Box ml={2} mt='1' fontWeight='normal' as='h4' lineHeight='tight' noOfLines={1}
+                                    >
+                                        {item.post_description}
+                                    </Box>
+                                </Box>
+                            </Box>
+                        }
                     </GridItem>
                 ))}
 
