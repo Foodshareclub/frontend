e1a879ce2d309364294f8b916a7d8822c6157a94
Denisssska <yarmoshkoden18m@gmail.com>
2023-02-07 18:22:51 +0300

    add chat center

--- src/components/productsLocation/ProductLocation.tsx
+++ src/components/productsLocation/ProductLocation.tsx
@@ -10,7 +10,6 @@ export const ProductsLocation = () => {
                 width={"100%"} height="100%"
                 style={{border: "0", borderRadius: "10px"}} loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade">
-
             </iframe>
         </Box>
 
