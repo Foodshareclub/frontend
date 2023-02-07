487dab18088c7a53d44842ae767443a8c81070c2
Denisssska <yarmoshkoden18m@gmail.com>
2023-02-07 22:00:16 +0300

    add chat settings

--- src/components/chatComponents/ContactsBlock.tsx
+++ src/components/chatComponents/ContactsBlock.tsx
@@ -1,7 +1,7 @@
 import {useAppSelector} from "@/hook";
 import {avatarURLSelector, userFirstNameSelector, userSecondNameSelector} from "@/store";
 import AvatarWithRipple from "@/components/listingPersonCard/AvatarWithRipple";
-import {Box, Flex, Heading, Input, InputGroup, InputLeftElement, Stack} from "@chakra-ui/react";
+import {Box, Flex, Heading, Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
 import {SearchIcon} from "@chakra-ui/icons";
 import {t} from "@lingui/macro";
 import {MinifiedUserInfo} from "@/components";
@@ -13,16 +13,16 @@ const ContactsBlock = () => {
     const userSecondName = useAppSelector(userSecondNameSelector);
 
     return (
-        <Box>
-            <AvatarWithRipple img={imgUrl}/>
+        <Flex direction={"column"} justify={"space-between"}>
             <Flex direction={"column"} alignSelf={"center"}>
+                <AvatarWithRipple img={imgUrl}/>
                 <Box py={2}>
                     <Heading textAlign={"center"} size='md'>
                         {userFirstName} {userSecondName}
                     </Heading>
                 </Box>
 
-                <InputGroup  alignSelf="center"  alignItems="center" flexDir="column">
+                <InputGroup alignSelf="center" alignItems="center" flexDir="column">
                     <InputLeftElement
                         pointerEvents={"stroke"}
                         children={<SearchIcon
@@ -30,12 +30,14 @@ const ContactsBlock = () => {
                     />
                     <Input
                         placeholder={t({
-                            id: `msg.Search people`,
-                            message: `Search people`
+                            id: `msg.Search...`,
+                            message: `Search...`
                         })}
                     />
                 </InputGroup>
-                <Box mt={10} bg={"gray.100"} borderRadius={"10%"} px={2} height={"300px"} overflow={"auto"}>
+            </Flex>
+            <Flex direction={"column"} alignSelf={"center"}>
+                <Box bg={"gray.100"} borderRadius={"10%"} px={2} height={"300px"} overflow={"auto"}>
                     <MinifiedUserInfo
                         src={imgUrl}
                         description={'SOME PRODUCT NAME'}
@@ -49,21 +51,6 @@ const ContactsBlock = () => {
                         secondName={'sName'}
                     />
                     <MinifiedUserInfo
-                        src={imgUrl}
-                        description={'SOME PRODUCT NAME'}
-                        firstName={'fNAME'}
-                        secondName={'sName'}
-                    /><MinifiedUserInfo
-                        src={imgUrl}
-                        description={'SOME PRODUCT NAME'}
-                        firstName={'fNAME'}
-                        secondName={'sName'}
-                    /><MinifiedUserInfo
-                        src={imgUrl}
-                        description={'SOME PRODUCT NAME'}
-                        firstName={'fNAME'}
-                        secondName={'sName'}
-                    /><MinifiedUserInfo
                         src={imgUrl}
                         description={'SOME PRODUCT NAME'}
                         firstName={'fNAME'}
@@ -73,11 +60,26 @@ const ContactsBlock = () => {
                     description={'SOME PRODUCT NAME'}
                     firstName={'fNAME'}
                     secondName={'sName'}
+                /><MinifiedUserInfo
+                    src={imgUrl}
+                    description={'SOME PRODUCT NAME'}
+                    firstName={'fNAME'}
+                    secondName={'sName'}
+                /><MinifiedUserInfo
+                    src={imgUrl}
+                    description={'SOME PRODUCT NAME'}
+                    firstName={'fNAME'}
+                    secondName={'sName'}
+                /><MinifiedUserInfo
+                    src={imgUrl}
+                    description={'SOME PRODUCT NAME'}
+                    firstName={'fNAME'}
+                    secondName={'sName'}
                 />
                 </Box>
 
             </Flex>
-        </Box>
+        </Flex>
     )
 }
 export default ContactsBlock;
