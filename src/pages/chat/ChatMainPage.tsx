9389e0b26c1b44e13e12dc6156af86021fe12681
Stanislav Lisovskiy <stas-lisovskii@mail.ru>
2023-02-08 11:18:28 +0300

    chat

--- src/pages/chat/ChatMainPage.tsx
+++ src/pages/chat/ChatMainPage.tsx
@@ -7,12 +7,14 @@ import {getOneProductTC} from "@/store/slices/productReducer";
 import ContactsBlock from "@/components/chatComponents/ContactsBlock";
 import {AddIcon} from "@chakra-ui/icons";
 import {supabase} from "@/supaBase.config";
+import {userIdFromSessionSelector} from "@/store";
 
 type MessagesType = {
     id: string
     created_at: Date
     content: string
     user_id: string
+    user_id_addressee: string
 }
 
 
@@ -33,7 +35,10 @@ const ChatMainPage = () => {
 //////////////////////////////////////////////////////////////////
     const [messages, setMessages] = useState<any>()
     useEffect(() => {
-        supabase.from("messages").select().then(res => setMessages(res.data))
+        supabase
+            .from("messages")
+            .select()
+            .then(res => setMessages(res.data))
     }, [])
 
     console.log(messages, "from table")
@@ -77,26 +82,34 @@ const ChatMainPage = () => {
             event: 'supa',
             payload: {org: val},
         }).then((res) => console.log(res))
+
+        setVal('');
     }
     /////////////////////////////////////////////
+
+    const userID = useAppSelector(userIdFromSessionSelector);
+    console.log(userID)
+
     return (
         <Flex justify={"space-between"} px={7} mt="24vh" mb={"12vh"}>
             <ContactsBlock/>
-            <Flex justify={"space-between"} direction={"column"} p={3} bg={"gray.200"} borderRadius={20} w={"50%"}>
-                <Box p={3} borderRadius={20} bg={"gray.100"} h={"92%"}>
-                    <Flex justify={"start"}>
-                        <Box bg={"white"} borderRadius={"25px"}
-                             maxWidth={"100px"}>
-                            <Text py={2} px={4}>Chat1</Text>
-                        </Box>
-                    </Flex>
-                    <Flex justify={"end"}>
-                        <Box bg={"red.100"} borderRadius={"25px"}
-                             maxWidth={"255px"}>
-                            <Text px={4} py={2}> Chat2</Text>
-                        </Box>
-                    </Flex>
-
+            <Flex justify={"space-between"} direction={"column"} p={3} bg={"gray.200"} borderRadius={20} w={"50%"} height={'500px'}>
+                <Box p={3} borderRadius={20} bg={"gray.100"} h={"90%"} overflow={"auto"}>
+                    {messages && messages.map((m: MessagesType) => {
+                        return userID === m.user_id
+                            ?<Flex justify={"end"}>
+                                <Box bg={"red.100"} borderRadius={"25px"}
+                                     maxWidth={"255px"}>
+                                    <Text px={4} py={2}>{m.content}</Text>
+                                </Box>
+                            </Flex>
+                            : <Flex justify={"start"}>
+                            <Box bg={"white"} borderRadius={"25px"}
+                                 maxWidth={"100px"}>
+                                <Text py={2} px={4}>{m.content}</Text>
+                            </Box>
+                        </Flex>
+                    })}
                 </Box>
                 <Flex borderRadius={20} h={"6%"} bg={"gray.200"}>
                     <Input _hover={{bg: "white"}} variant={"filled"} borderRadius={20}
