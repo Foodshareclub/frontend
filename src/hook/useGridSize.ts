1c41ba457a33d1773db00a147977df78a528e6d3
Denisssska <yarmoshkoden18m@gmail.com>
2023-03-03 21:24:06 +0300

    refactor  style

--- src/hook/useGridSize.ts
+++ src/hook/useGridSize.ts
@@ -1,17 +1,17 @@
 import {useMediaQuery} from "@/hook/index";
 
 export const useGridSize = (): number | undefined => {
-    const isSmallerThan500 = useMediaQuery('(min-width:500px)');
+    const isSmallerThan550 = useMediaQuery('(min-width:550px)');
     const isSmallerThan700 = useMediaQuery('(min-width:700px)');
     const isSmallerThan1290 = useMediaQuery('(min-width:1290px)');
 
     if (isSmallerThan1290) {
-        return 5;
-    }
-    if (isSmallerThan700) {
         return 4;
     }
-    if (isSmallerThan500) {
+    if (isSmallerThan700) {
+        return 3;
+    }
+    if (isSmallerThan550) {
         return 2;
     }
 };
